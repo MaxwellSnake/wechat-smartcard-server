9a530188e866572e68b7c0f7a4f23c1331627275
Maxwell <769474321@qq.com>
2017-02-05 23:14:02 +0800

    delete control.py

--- control.py
+++
@@ -1,5 +0,0 @@
-# _*_ coding:utf-8 _*_
-import time
-def  register(weixin,fromUser,toUser,content,reply):
-    if content == 'test':
-        return weixin.render.reply_text(fromUser, toUser, int(time.time()), reply)
